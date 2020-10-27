af0bdbb5198a7ee5047487c099a6f3bdf6017b92
elkinarnoldo <73562754+elkinarnoldo@users.noreply.github.com>
2020-10-27 18:20:41 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,114 @@
+basic.forever(function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . # # #
+        . # . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . # #
+        . . # . #
+        . # . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        . . # . #
+        . # . . #
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        . . # . #
+        . # . . #
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        . . # . #
+        . # . # .
+        # # # . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        . . # # .
+        . # # . .
+        # # . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . #
+        . . . # .
+        . . # . .
+        . # . . .
+        # . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . #
+        . . . # .
+        . . # . .
+        . # . . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        . . . # .
+        . . # . .
+        . # . . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        . . . # .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000)
+})
